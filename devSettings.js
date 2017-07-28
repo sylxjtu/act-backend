bd3c104200e7c31c391695418ecfbcc378145f4b
sylxjtu <a443833359@gmail.com>
2017-07-28 20:12:25 +0800

    seperate settings

---
+++ devSettings.js
@@ -0,0 +1,19 @@
+module.exports = {
+  port: 3000,
+  mysqlPool: {
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    database: 'activity'
+  },
+  session: {
+    secret: "sylxjtu",
+    resave: false,
+    saveUninitialized: false,
+  },
+  cors: {
+    origin: "http://localhost:8080",
+    credentials: true
+  },
+  storeConfig: {}
+};
