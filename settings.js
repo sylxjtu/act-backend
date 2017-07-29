dee8d5ffa1a15cab7b4ace183d3dd55f6c031707
sylxjtu <a443833359@gmail.com>
2017-07-29 11:57:42 +0800

    fix seperate setting

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-const productionSettings = require('productionSettings');
-const devSettings = require('devSettings');
+const productionSettings = require('./productionSettings');
+const devSettings = require('./devSettings');
 module.exports = (process.env.NODE_ENV === 'production' ? productionSettings : devSettings);
